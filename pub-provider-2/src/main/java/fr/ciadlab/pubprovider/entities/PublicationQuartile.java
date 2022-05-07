805bed3a2cb81bdb5f1ee92be53f64ddf8802602
Zapolatero <pascuzzilouis@gmail.com>
2022-05-07 17:43:24 +0200

    Ajout de l'énumération PublicationQuartile

---
+++ pub-provider-2/src/main/java/fr/ciadlab/pubprovider/entities/PublicationQuartile.java
@@ -0,0 +1,14 @@
+package fr.ciadlab.pubprovider.entities;
+
+/**
+ * @author pascuzzi louis
+ *
+ * Quartiles specify a publication's relevance level.
+ * Quartiles range from Q1 (less relevant) to Q4 (most relevant)
+ */
+public enum PublicationQuartile {
+	Q1,
+	Q2,
+	Q3,
+	Q4
+}
