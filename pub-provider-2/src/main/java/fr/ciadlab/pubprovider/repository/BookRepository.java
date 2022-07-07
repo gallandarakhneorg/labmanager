0781acc30c1f51bb50b00bb73ff3d2c30fa7f816
Stéphane Galland <galland@arakhne.org>
2022-07-07 10:34:22 +0200

    Add standard file headers.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- pub-provider-2/src/main/java/fr/ciadlab/pubprovider/repository/BookRepository.java
+++ pub-provider-2/src/main/java/fr/ciadlab/pubprovider/repository/BookRepository.java
@@ -1,3 +1,18 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2019-22, CIAD Laboratory, Universite de Technologie de Belfort Montbeliard
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of the Systems and Transportation Laboratory ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with the SeT.
+ * 
+ * http://www.ciad-lab.fr/
+ */
+
 package fr.ciadlab.pubprovider.repository;
 
 import fr.ciadlab.pubprovider.entities.Book;
