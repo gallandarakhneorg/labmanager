223ffb5e3cc1d0ef18461effee3644b4a0c18e3d
Stéphane Galland <galland@arakhne.org>
2022-07-30 22:53:34 +0200

    Replace "SeT" by "CIAD".
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/io/ExporterConfigurator.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/io/ExporterConfigurator.java
@@ -8,7 +8,7 @@
  * of the Systems and Transportation Laboratory ("Confidential Information").
  * You shall not disclose such Confidential Information and shall use
  * it only in accordance with the terms of the license agreement
- * you entered into with the SeT.
+ * you entered into with the CIAD.
  * 
  * http://www.ciad-lab.fr/
  */
