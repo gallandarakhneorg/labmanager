1f3b36f1645c9661e8591667cca19bb8d1a30b0d
Stéphane Galland <galland@arakhne.org>
2022-08-02 02:48:04 +0200

    Add gender and orcid to the persons.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

---
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/entities/member/Gender.java
@@ -0,0 +1,61 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2019-22, CIAD Laboratory, Universite de Technologie de Belfort Montbeliard
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of the Systems and Transportation Laboratory ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with the CIAD.
+ * 
+ * http://www.ciad-lab.fr/
+ */
+
+package fr.ciadlab.labmanager.entities.member;
+
+import com.google.common.base.Strings;
+
+/** Gender of a person.
+ * 
+ * @author $Author: sgalland$
+ * @version $Name$ $Revision$ $Date$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ * @since 2.0.0
+ */
+public enum Gender {
+	/** Unknown gender.
+	 */
+	NOT_SPECIFIED,
+	/** Male.
+	 */
+	MALE,
+	/** Female.
+	 */
+	FEMALE,
+	/** Other gender.
+	 */
+	OTHER;
+
+
+	/** Replies the gender that corresponds to the given name, with a case-insensitive
+	 * test of the name.
+	 *
+	 * @param name the name of the gender, to search for.
+	 * @return the status.
+	 * @throws IllegalArgumentException if the given name does not corresponds to a type.
+	 */
+	public static Gender valueOfCaseInsensitive(String name) {
+		if (!Strings.isNullOrEmpty(name)) {
+			for (final Gender gender : values()) {
+				if (name.equalsIgnoreCase(gender.name())) {
+					return gender;
+				}
+			}
+		}
+		throw new IllegalArgumentException("Invalid gender: " + name); //$NON-NLS-1$
+	}
+
+}
