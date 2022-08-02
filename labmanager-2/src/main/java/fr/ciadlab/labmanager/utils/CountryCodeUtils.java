f5cd2bb8fa7340b954f40397cd2382e7a3ec43e4
Stéphane Galland <galland@arakhne.org>
2022-08-02 02:51:14 +0200

    Add country code utilities.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

---
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/utils/CountryCodeUtils.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) 2019-22, CIAD Laboratory, Universite de Technologie de Belfort Montbeliard
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of the Systems and Transportation Laboratory ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with the CIAD.
+ * 
+ * http://www.ciad-lab.fr/
+ */
+
+package fr.ciadlab.labmanager.utils;
+
+import java.util.Locale;
+
+import com.google.common.base.Strings;
+import org.arakhne.afc.util.CountryCode;
+
+/** Utilities for country codes.
+ * 
+ * @author $Author: sgalland$
+ * @version $Name$ $Revision$ $Date$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ * @since 2.0.0
+ */
+public final class CountryCodeUtils {
+
+	private CountryCodeUtils() {
+		//
+	}
+
+	/** Replies the country code that corresponds to the given name, with a case-insensitive
+	 * test of the name.
+	 *
+	 * @param name the name of the code, to search for.
+	 * @return the code.
+	 * @throws IllegalArgumentException if the given name does not corresponds to a type.
+	 */
+	public static CountryCode valueOfCaseInsensitive(String name) {
+		if (!Strings.isNullOrEmpty(name)) {
+			for (final CountryCode code : CountryCode.values()) {
+				if (name.equalsIgnoreCase(code.name())) {
+					return code;
+				}
+			}
+		}
+		throw new IllegalArgumentException("Invalid country code: " + name); //$NON-NLS-1$
+	}
+
+
+	/** Replies the display name of the country.
+	 *
+	 * @param code the country code to use.
+	 * @return the display name of the country.
+	 */
+	public static String getDisplayCountry(CountryCode code) {
+		return new Locale(code.getCode(), code.getCode()).getDisplayCountry();
+	}
+
+}
