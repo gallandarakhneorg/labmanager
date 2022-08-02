777247240262e5de848a3e9040d14d064c5a5695
Stéphane Galland <galland@arakhne.org>
2022-08-02 20:12:09 +0200

    Fixing the "/organizationList" endpoint.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/repository/organization/ResearchOrganizationRepository.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/repository/organization/ResearchOrganizationRepository.java
@@ -15,6 +15,8 @@
 
 package fr.ciadlab.labmanager.repository.organization;
 
+import java.util.Optional;
+
 import fr.ciadlab.labmanager.entities.organization.ResearchOrganization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -28,5 +30,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @see ResearchOrganization
  */
 public interface ResearchOrganizationRepository extends JpaRepository<ResearchOrganization, Integer> {
-	//
+
+	/** Find a research organization with the given acronm or name.
+	 *
+	 * @param acronym the acronym to search for.
+	 * @param name the name to search for.
+	 * @return the research organization.
+	 */
+	Optional<ResearchOrganization> findDistinctByAcronymOrName(String acronym, String name);
+
 }
