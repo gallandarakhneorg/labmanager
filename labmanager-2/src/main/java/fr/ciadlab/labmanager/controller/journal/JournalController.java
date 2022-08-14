87536813509ce1090387e9f08aea452700cbc54c
Stéphane Galland <galland@arakhne.org>
2022-08-14 16:36:46 +0200

    Add endpoint '/getJournalQualityIndicators' and a download link in the list of journals.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/journal/JournalController.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/journal/JournalController.java
@@ -17,9 +17,13 @@
 package fr.ciadlab.labmanager.controller.journal;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import fr.ciadlab.labmanager.controller.AbstractController;
 import fr.ciadlab.labmanager.entities.journal.Journal;
+import fr.ciadlab.labmanager.entities.journal.JournalQualityAnnualIndicators;
 import fr.ciadlab.labmanager.service.journal.JournalService;
 import org.apache.jena.ext.com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -89,108 +93,111 @@ public class JournalController extends AbstractController {
 		if (id == null && Strings.isNullOrEmpty(name)) {
 			throw new IllegalArgumentException("Name and identifier parameters are missed"); //$NON-NLS-1$
 		}
-		if (Strings.isNullOrEmpty(name)) {
-			assert id != null;
+		if (id != null) {
 			return this.journalService.getJournalById(id.intValue());
 		}
 		return this.journalService.getJournalByName(name);
 	}
 
-//	/** Add a journal into the database.
-//	 *
-//	 * @param response JEE response.
-//	 * @param name the name of the journal.
-//	 * @param publisher the name of the publisher of the journal.
-//	 * @param url the URL to the page of the journal on the publisher website.
-//	 * @param scimagoId the identifier to the page of the journal on the Scimago website.
-//	 * @param wosId the identifier to the page of the journal on the Web-Of-Science website.
-//	 * @throws Exception if the redirection to success/failure page cannot be done.
-//	 */
-//	@PostMapping("/addJournal")
-//	public void addJournal(HttpServletResponse response,
-//			@RequestParam String name,
-//			@RequestParam String publisher,
-//			@RequestParam String url,
-//			@RequestParam String scimagoId,
-//			@RequestParam String wosId) throws Exception {
-//		try {
-//			this.journalService.createJournal(
-//					name, publisher, url, scimagoId, wosId);
-//			redirectCreated(response, name);
-//		} catch (Exception ex) {
-//			redirectError(response, ex);
-//		}
-//
-//	}
-//
-//	/** Replies the quality indicators for a specific journal.
-//	 *
-//	 * @param year the year for which the indicators must be replied.
-//	 * @param name the name of the journal.
-//	 * @return the quality indicators, or {@code null} if no indicator is available.
-//	 */
-//	@GetMapping("/getJournalQualityIndicators")
-//	public @ResponseBody Map<String, String> getJournalQualityIndicators(
-//			@RequestParam int year,
-//			@RequestParam String name) {
-//		final Journal journal = this.journalService.getJournalByName(name);
-//		if (journal != null && journal.hasQualityIndicatorsForYear(year)) {
-//			// creation of a JSON object containing each of the journal's quality indicators
-//			final Map<String, String> journalIndicators = new HashMap<>();
-//
-//			// Get of the individual indicators
-//			final QuartileRanking scimago = journal.getScimagoQIndexByYear(year);
-//			final QuartileRanking wos = journal.getWosQIndexByYear(year);
-//			final float impactFactor = journal.getImpactFactorByYear(year);
-//
-//			// Addition of the indicators in the json map
-//			if (scimago != null) {
-//				journalIndicators.put("scimagoQuartile", scimago.name()); //$NON-NLS-1$
-//			}
-//			if (wos != null) {
-//				journalIndicators.put("wosQuartile", wos.name()); //$NON-NLS-1$
-//			}
-//			if (impactFactor != 0) {
-//				journalIndicators.put("impactFactor", Float.toString(impactFactor)); //$NON-NLS-1$
-//			}
-//
-//			if (!journalIndicators.isEmpty()) {
-//				return journalIndicators;
-//			}
-//		}
-//		return null;
-//	}
-//
-//	/** Edit the fields of a journal in the database.
-//	 *
-//	 * @param response the HTTP response.
-//	 * @param journal original name of the journal that allows to find it in the database. 
-//	 * @param name the name of the journal.
-//	 * @param publisher the name of the publisher of the journal.
-//	 * @param url the URL to the page of the journal on the publisher website.
-//	 * @param scimagoId the identifier to the page of the journal on the Scimago website.
-//	 * @param wosId the identifier to the page of the journal on the Web-Of-Science website.
-//	 * @throws Exception if the redirection to success/failure page cannot be done.
-//	 */
-//	@PostMapping("/editJournal")
-//	public void editJournal(HttpServletResponse response,
-//			@RequestParam String journal,
-//			@RequestParam String name,
-//			@RequestParam String publisher,
-//			@RequestParam String url,
-//			@RequestParam String scimagoId,
-//			@RequestParam String wosId) throws Exception {
-//		try {
-//			final int journalId = this.journalService.getJournalIdByName(journal);
-//			if (journalId != 0) {
-//				this.journalService.updateJournal(journalId, name, publisher, url, scimagoId, wosId);
-//				redirectUpdated(response, journal);
-//			} else {
-//				redirectError(response, this.messages.getMessage(MESSAGES_PREFIX + "NO_JOURNAL_ERROR", new Object[] {journal})); //$NON-NLS-1$
-//			}
-//		} catch (Exception ex) {
-//			redirectError(response, ex);
-//		}
-//	}
+	/** Replies the quality indicators for a specific journal.
+	 * This endpoint accepts one of the two parameters: the name or the identifier of the journal.
+	 * If the years are provided, only the quality indicators for these years are replied. Otherwise,
+	 * all the quality indicators are replied.
+	 *
+	 * @param name the name of the journal.
+	 * @param id the identifier of the journal.
+	 * @param years the list of years for which the indicators must be replied.
+	 * @return a map of quality indicators per year.
+	 */
+	@GetMapping(value = "/getJournalQualityIndicators", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public Map<Integer, JournalQualityAnnualIndicators> getJournalQualityIndicators(
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) Integer id,
+			@RequestParam(required = false, name = "year") List<Integer> years) {
+		if (id == null && Strings.isNullOrEmpty(name)) {
+			throw new IllegalArgumentException("Name and identifier parameters are missed"); //$NON-NLS-1$
+		}
+		final Journal journal;
+		if (id != null) {
+			journal = this.journalService.getJournalById(id.intValue());
+		} else {
+			journal = this.journalService.getJournalByName(name);
+		}
+		if (journal == null) {
+			throw new IllegalArgumentException("Journal not found"); //$NON-NLS-1$
+		}
+		if (years != null && !years.isEmpty()) {
+			final Map<Integer, JournalQualityAnnualIndicators> indicators = new HashMap<>();
+			for (final Integer year : years) {
+				if (year != null) {
+					indicators.computeIfAbsent(year, it -> {
+						return journal.getQualityIndicatorsFor(year.intValue(), null);
+					});
+				}
+			}
+			return indicators;
+		}
+		return journal.getQualityIndicators();
+	}
+
+	//	/** Add a journal into the database.
+	//	 *
+	//	 * @param response JEE response.
+	//	 * @param name the name of the journal.
+	//	 * @param publisher the name of the publisher of the journal.
+	//	 * @param url the URL to the page of the journal on the publisher website.
+	//	 * @param scimagoId the identifier to the page of the journal on the Scimago website.
+	//	 * @param wosId the identifier to the page of the journal on the Web-Of-Science website.
+	//	 * @throws Exception if the redirection to success/failure page cannot be done.
+	//	 */
+	//	@PostMapping("/addJournal")
+	//	public void addJournal(HttpServletResponse response,
+	//			@RequestParam String name,
+	//			@RequestParam String publisher,
+	//			@RequestParam String url,
+	//			@RequestParam String scimagoId,
+	//			@RequestParam String wosId) throws Exception {
+	//		try {
+	//			this.journalService.createJournal(
+	//					name, publisher, url, scimagoId, wosId);
+	//			redirectCreated(response, name);
+	//		} catch (Exception ex) {
+	//			redirectError(response, ex);
+	//		}
+	//
+	//	}
+	//
+	//	/** Edit the fields of a journal in the database.
+	//	 *
+	//	 * @param response the HTTP response.
+	//	 * @param journal original name of the journal that allows to find it in the database. 
+	//	 * @param name the name of the journal.
+	//	 * @param publisher the name of the publisher of the journal.
+	//	 * @param url the URL to the page of the journal on the publisher website.
+	//	 * @param scimagoId the identifier to the page of the journal on the Scimago website.
+	//	 * @param wosId the identifier to the page of the journal on the Web-Of-Science website.
+	//	 * @throws Exception if the redirection to success/failure page cannot be done.
+	//	 */
+	//	@PostMapping("/editJournal")
+	//	public void editJournal(HttpServletResponse response,
+	//			@RequestParam String journal,
+	//			@RequestParam String name,
+	//			@RequestParam String publisher,
+	//			@RequestParam String url,
+	//			@RequestParam String scimagoId,
+	//			@RequestParam String wosId) throws Exception {
+	//		try {
+	//			final int journalId = this.journalService.getJournalIdByName(journal);
+	//			if (journalId != 0) {
+	//				this.journalService.updateJournal(journalId, name, publisher, url, scimagoId, wosId);
+	//				redirectUpdated(response, journal);
+	//			} else {
+	//				redirectError(response, this.messages.getMessage(MESSAGES_PREFIX + "NO_JOURNAL_ERROR", new Object[] {journal})); //$NON-NLS-1$
+	//			}
+	//		} catch (Exception ex) {
+	//			redirectError(response, ex);
+	//		}
+	//	}
 
 }
