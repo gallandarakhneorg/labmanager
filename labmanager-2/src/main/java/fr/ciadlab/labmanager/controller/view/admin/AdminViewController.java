b766340b075859a4afbc8cdcee1d2c7d21204f0c
Stéphane Galland <galland@arakhne.org>
2022-09-03 15:12:45 +0200

    Add dropdown buttons on the admin page.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/view/admin/AdminViewController.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/controller/view/admin/AdminViewController.java
@@ -16,9 +16,21 @@
 
 package fr.ciadlab.labmanager.controller.view.admin;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import fr.ciadlab.labmanager.configuration.Constants;
+import fr.ciadlab.labmanager.controller.view.AbstractViewController;
+import fr.ciadlab.labmanager.entities.EntityUtils;
+import fr.ciadlab.labmanager.entities.organization.ResearchOrganization;
+import fr.ciadlab.labmanager.service.organization.ResearchOrganizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /** This general controller shows up the administration tools and the list of all the controllers in the backend.
  * 
@@ -28,17 +40,40 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @mavenartifactid $ArtifactId$
  * @since 2.0.0
  */
-@Controller
-public class AdminViewController {
+@RestController
+@CrossOrigin
+public class AdminViewController extends AbstractViewController {
 
-	/** Shows up the HTML page for the administration entry point.
+	private ResearchOrganizationService organizationService;
+
+	/** Constructor for injector.
+	 * This constructor is defined for being invoked by the IOC injector.
 	 *
-	 * @param model the model.
-	 * @return not used.
+	 * @param messages the provider of messages.
+	 * @param constants the accessor to the live constants.
+	 * @param organizationService the research organization service.
+	 */
+	public AdminViewController(
+			@Autowired MessageSourceAccessor messages,
+			@Autowired Constants constants,
+			@Autowired ResearchOrganizationService organizationService) {
+		super(messages, constants);
+		this.organizationService = organizationService;
+	}
+
+	/** Shows up the main administration page.
+	 *
+	 * @param username the login of the logged-in person.
+	 * @return the model-view of the list of publications.
 	 */
 	@GetMapping(value = { "/", "/index.html", "index.htm", "/index.php", "/index.jsp", "admin" })
-	public static String admin(Model model) {
-		return "admin"; //$NON-NLS-1$
+	public ModelAndView admin(@CurrentSecurityContext(expression="authentication?.name") String username) {
+		final ModelAndView modelAndView = new ModelAndView("admin"); //$NON-NLS-1$
+		initModelViewProperties(modelAndView, username);
+		final List<ResearchOrganization> list = this.organizationService.getAllResearchOrganizations().stream()
+				.sorted(EntityUtils.getPreferredResearchOrganizationComparator()).collect(Collectors.toList());
+		modelAndView.addObject("organizations", list); //$NON-NLS-1$
+		return modelAndView;
 	}
 
 }
