53651b75e0eb39583ee33a9ad090f97d95696b5a
Stéphane Galland <galland@arakhne.org>
2023-11-28 22:44:42 +0800

    Add AGPL-3.0 license.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- labmanager-2/src/main/java/fr/ciadlab/labmanager/service/conference/ConferenceService.java
+++ labmanager-2/src/main/java/fr/ciadlab/labmanager/service/conference/ConferenceService.java
@@ -2,16 +2,19 @@
  * $Id$
  * 
  * Copyright (c) 2019-2024, CIAD Laboratory, Universite de Technologie de Belfort Montbeliard
- * All rights reserved.
  *
- * This software is the confidential and proprietary information
- * of the CIAD laboratory and the Université de Technologie
- * de Belfort-Montbéliard ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with the CIAD-UTBM.
- * 
- * http://www.ciad-lab.fr/
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package fr.ciadlab.labmanager.service.conference;
