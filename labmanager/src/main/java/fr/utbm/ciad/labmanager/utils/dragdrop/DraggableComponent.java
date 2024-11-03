ca7a686ac0290e3c9b6fb51d9358b3f31b76b236
Pierre Schneiderlin <pierre.schneiderlin@utbm.fr>
2024-11-04 00:40:56 +0100

    Improve the components' resize functionality

--- labmanager/src/main/java/fr/utbm/ciad/labmanager/utils/dragdrop/DraggableComponent.java
+++ labmanager/src/main/java/fr/utbm/ciad/labmanager/utils/dragdrop/DraggableComponent.java
@@ -4,25 +4,27 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.dnd.DragSource;
 import com.vaadin.flow.component.html.Div;
 
-
 public class DraggableComponent extends Div {
 
     private Component component;
 
-    /** Constructor for DraggableComponent.
+    /**
+     * Constructor for DraggableComponent.
      *
      * @param component the component to make draggable.
      * @param dropGrid the DropGrid that will handle the drag-and-drop functionality.
      */
     public DraggableComponent(Component component, DropGrid dropGrid) {
         setComponent(component);
-        getStyle().set("z-index", "999");
+        getStyle().set("z-index", "2");
         getStyle().set("resize", "both");
         getStyle().set("overflow", "hidden");
         getStyle().set("position", "absolute");
 
         DragSource<Component> dragSource = DragSource.create(this);
         dragSource.setDraggable(true);
+
+        addResizeListener();
 
         // Listener for when dragging starts
         dragSource.addDragStartListener(event -> {
@@ -43,32 +45,73 @@ public class DraggableComponent extends Div {
                 getStyle().set("border", "none"));
     }
 
-    /** Retrieves the current UI component.
-     * This method resets certain style properties before returning
-     * the component, ensuring it has the correct dimensions and layering.
+    /**
+     * Adds a resize listener to the component that dispatches a custom resize event
+     * whenever the size of the component changes.
+     */
+    private void addResizeListener(){
+        String jsCode =
+                "const resizeObserver = new ResizeObserver(entries => {" +
+                        "   for (let entry of entries) {" +
+                        "       this.dispatchEvent(new Event('custom-resize')); " +
+                        "   }" +
+                        "});" +
+                        "resizeObserver.observe(this);";
+
+        getElement().executeJs(jsCode);
+
+        getElement().addEventListener("custom-resize", event -> adaptComponentSize());
+    }
+
+    /**
+     * Adapts the size of the wrapped component based on the size of the DraggableComponent.
+     * This method retrieves the current width and height of the component and updates
+     * the dimensions of the wrapped component accordingly.
+     */
+    private void adaptComponentSize() {
+        getElement().executeJs("return this.offsetWidth;")
+                .then(width -> {
+                    component.getStyle()
+                            .set("width", width.asString() + "px");
+                    getElement().executeJs("return this.offsetHeight;")
+                            .then(height -> component.getStyle()
+                                    .set("height", height.asString() + "px"));
+                });
+    }
+
+    /**
+     * Retrieves the current UI component.
      *
      * @return the current Component.
      */
     public Component getComponent() {
         Component component = this.component;
-        component.getStyle().remove("z-index");
-        component.getStyle().set("width", getStyle().get("width"));
-        component.getStyle().set("height", getStyle().get("height"));
+
         component.getElement().setAttribute("data-custom-id", getElement().getAttribute("data-custom-id"));
         return component;
     }
 
+    /**
+     * Sets the component to be made draggable.
+     * This method initializes the component's style properties
+     * and adds it to the DraggableComponent.
+     *
+     * @param component the component to set.
+     */
     private void setComponent(Component component){
         if(this.component == null){
-            if(component.getElement().getAttribute("data-custom-id") != null){
-                getElement().setAttribute("data-custom-id", component.getElement().getAttribute("data-custom-id"));
+            component.getStyle().set("z-index", "1");
+            String componentName = component.getElement().getAttribute("data-custom-id");
+            if(componentName != null){
+                getElement().setAttribute("data-custom-id", componentName);
             }
-            getStyle().setWidth(component.getStyle().get("width")).setHeight(component.getStyle().get("height"));
-            component.getStyle().set("width", "100%");
-            component.getStyle().set("height", "100%");
-            component.getStyle().set("z-index", "0");
+            getStyle().setWidth(component.getStyle().get("width"))
+                    .setHeight(component.getStyle().get("height"));
+
             this.component = component;
             add(this.component);
         }
     }
 }
+
+
