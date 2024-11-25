80f8e2f5cb48be7c70a7af1f96a9d9ffcefbb56d
Jérémie KIMENAU <jkimenau123@gmail.com>
2024-11-25 16:35:35 +0100

    move data classes to respective files

--- labmanager/src/main/java/fr/utbm/ciad/wprest/publications/PublicationRestService.java
+++ labmanager/src/main/java/fr/utbm/ciad/wprest/publications/PublicationRestService.java
@@ -10,6 +10,7 @@ import fr.utbm.ciad.labmanager.services.member.PersonService;
 import fr.utbm.ciad.labmanager.services.organization.ResearchOrganizationService;
 import fr.utbm.ciad.labmanager.services.publication.PublicationService;
 import fr.utbm.ciad.wprest.data.PersonOnWebsite;
+import fr.utbm.ciad.wprest.publications.data.dto.PublicationsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -227,32 +228,5 @@ public class PublicationRestService {
         }
 
         return publications;
-    }
-
-
-    /**
-     * Data Transfer Object (DTO) representing a publication's information.
-     *
-     * @param title           the title of the publication
-     * @param doi             the doi of the publication
-     * @param issn            the issn of the publication
-     * @param publicationDate the date the publication was released
-     * @param publicationType the type of the publication (e.g., journal article, book, etc.)
-     * @param persons         a list of names of persons associated with the publication
-     * @param abstractText    a brief summary of the publication's content
-     * @param pdfUrl          the URL link to the PDF version of the publication
-     * @param language        the language in which the publication is written
-     * @param keywords        a list of keywords related to the publication
-     */
-    public record PublicationsDTO(String title,
-                                  String doi,
-                                  String issn,
-                                  LocalDate publicationDate,
-                                  PublicationType publicationType,
-                                  List<PersonOnWebsite> persons,
-                                  String abstractText,
-                                  String pdfUrl,
-                                  PublicationLanguage language,
-                                  List<String> keywords) {
     }
 }
